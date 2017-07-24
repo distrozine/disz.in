d0182e56736f74b3221ac3c63fc7550665275d02
xtsidx <xtsidx@gmail.com>
2017-07-24 22:38:24 +0300

    Fix and new i18n path helpers

--- lib/helpers/i18n.rb
+++ lib/helpers/i18n.rb
@@ -4,19 +4,24 @@ module I18nHelpers
 	  return nil unless value
 	  return value if value.is_a? String
 	  value[::I18n.locale] || value[::I18n.default_locale] || value.first
-	end
+  end
+
+  # remove locale prefix from path
+  def unlocalize_path(path = current_page.path)
+    exists_locale = path[/^\/?([a-z]{2})\//,1]
+    exists_locale = nil unless langs.include? exists_locale&.to_sym
+    exists_locale ? path[3..-1] : path
+  end
 
 	# add or change locale prefix if need
 	def localize_path(locale, path = current_page.path)
-	  exists_locale = path[/^?(..)\//,1]
-	  exists_locale = nil unless langs.include? exists_locale&.to_sym
-	  unlocale_path = exists_locale ? path[3..-1] : path
+    unlocale_path = unlocalize_path(path)
 	  ::I18n.default_locale == locale ? unlocale_path : "/#{locale}#{unlocale_path}"
 	end
 
 	# get locale from path
 	def locale_from_path(path = current_page.path)
-		exists_locale = path[/^?(..)\//,1]
+		exists_locale = path[/^\/?([a-z]{2})\//,1]
 		exists_locale = nil unless langs.include? exists_locale&.to_sym
 		(exists_locale  ||  ::I18n.default_locale).to_sym
 	end
